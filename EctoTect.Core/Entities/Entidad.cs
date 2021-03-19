c9bce15188a48a5812d2e7d8783045fe0c726efe
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2021-03-19 03:31:22 -0600

    Entities

---
+++ EctoTect.Core/Entities/Entidad.cs
@@ -0,0 +1,17 @@
+﻿namespace EctoTect.Core.Entities
+{
+    public class Entidad
+    {
+        public Entidad()
+        {
+            Ciudad = new HashSet<Ciudad>();
+        }
+
+        public int Id { get; set; }
+        public int IdPais { get; set; }
+        public string Nombre { get; set; }
+
+        public virtual Pais IdPaisNavigation { get; set; }
+        public virtual ICollection<Ciudad> Ciudad { get; set; }
+    }
+}
---
+++ EctoTect.Core/Entities/Pais.cs
@@ -0,0 +1,16 @@
+﻿namespace EctoTect.Core.Entities
+{
+    using System.Collections.Generic;
+    public class Pais
+    {
+        public Pais()
+        {
+            Entidad = new HashSet<Entidad>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public virtual ICollection<Entidad> Entidad { get; set; }
+    }
+}
